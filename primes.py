1cf5f85aab4ab2f380e4d855a26106ae109c6707
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-09-28 10:41:29 +0100

    Completed task

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError
     list = []
+    i = 2
+    while len(list) < number_of_primes and i < 1000:
+        # for possiblePrime in range(2, 21):
+            isPrime = True
+            for num in range(2, i):
+                if i % num == 0:
+                    isPrime = False
+                    break
+            if isPrime == True:
+                list.append(i)
+            i = i + 1
+
     return list
